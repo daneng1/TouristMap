42a496cfa0418193563032497f4f455621fdc233
daneng1 <dan.engel@carerev.com>
2023-06-07 15:26:28 -0700

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Dan Engel on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
